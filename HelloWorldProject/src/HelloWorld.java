be1f95dc1d5e50484c35160c1e1a6cfc916116ba
TahrinH <Tahrinhaque1@gmail.com>
2025-09-09 15:01:29 +0000

    ready to grade

--- HelloWorldProject/src/HelloWorld.java
+++ HelloWorldProject/src/HelloWorld.java
@@ -1,13 +1,14 @@
 /*
- * TODO: Name
- * TODO: Date
- * TODO: Class Period
+ * TODO: Tahrin 
+ * TODO: 9/9/25
+ * TODO:4
  * TODO: Program Description
  */
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		//TODO: Implement Program Requirements Here
-	}
+			System.out.println("Hello World!");
 
+	}
 }
